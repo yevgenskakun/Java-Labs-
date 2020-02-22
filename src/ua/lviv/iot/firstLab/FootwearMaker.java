c62a76bd9d8656b0b201b4b922c2034a604bb633
yevgenskakun <56487723+yevgenskakun@users.noreply.github.com>
2020-02-22 13:55:53 +0200

    Lab_1 with basic tasks

---
+++ src/ua/lviv/iot/firstLab/FootwearMaker.java
@@ -0,0 +1,28 @@
+package ua.lviv.iot.firstLab;
+
+public class FootwearMaker {
+
+	public static void main(String[] args) {
+		
+		Footwear Nikefootwear = new Footwear("Nike", 120, 99, "Sniekers", "Israel");
+		
+		Footwear Adidasfootwear = new Footwear("Adidas", 110, 199, "Slippers", "China");
+		
+		Footwear.setColor("black");
+		
+		
+		System.out.println(Nikefootwear);
+		System.out.println("---------------------");
+		System.out.println(Adidasfootwear);
+		System.out.println("---------------------");
+		
+//        Footwear.printStaticNumberInTheGroup();
+//        System.out.println("----------------Static method----------------\n");
+//        
+//        Footwear.printNumberInTheGroup();
+//        System.out.println("----------------NON-Static method----------------\n");
+//		
+	}
+
+}
+ 
